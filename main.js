4b98d257783abd170e30ab1270112846bcb6c666
Josiah Plett <jlplett303@gmail.com>
2023-10-26 20:04:49 -0400

    Last sort of skeleton initialization

--- main.js
+++ main.js
@@ -77,116 +77,83 @@ let Assets = {
 
 let Animator = {
   frame: 0,
-  fps, fpsInterval, startTime, now, then, elapsed,
+  paused: false,
+  fpsInterval, startTime, now, then, elapsed,
 
-  startAnimating: () => {
+  startAnimating: (fps) => {
     fpsInterval = 1000 / fps;
     then = Date.now();
     startTime = then;
-    animate();
-  }
-}
-// To run actual frame-by-frame animation
-var stop = false;
-var frameCount = 0;
-var fps, fpsInterval, startTime, now, then, elapsed;
-
-function startAnimating(fps) {
-    fpsInterval = 1000 / fps;
-    then = Date.now();
-    startTime = then;
-    animate();
-}
-
-function animate() {
+    this.animate();
+  },
+  animate: () => {
     requestAnimationFrame(animate);
     now = Date.now();
     elapsed = now - then;
-    // if enough time has elapsed, draw the next frame
-    if (elapsed > fpsInterval) {
-        then = now - (elapsed % fpsInterval);
-        // actual looping code below!
-        if (!paused) {
-            avatar.physics();
-            frame += time;
-            if (!(frame % Math.round(60 / stepSpeed))) {
-                stepCounter++;
-                step += time;
-            }
-            let buttons = levels.buttons[levels.currentLevel].length;
-            let canSwap = levels.powers[levels.currentLevel][0];
-            if (!canSwap && buttons) {
-                for (let i = 0; i < levels.buttons[levels.currentLevel].length; i++) {
-                    if (levels.buttons[levels.currentLevel][i].type == 2) {
-                        canSwap = 1;
-                    }
-                }
-            }
 
-            if (avatar.moved) levels.updateTime();
-            else levels.updateTime(true);
-
-            if (!(frame % GFuel) && canSwap || buttons) { // Run the Ghosts + Update the Level objects
-                levels.update();
-                if (canSwap) {
-                    nextGhost.learn();
-                    clear(4);
-                    for (g in levels.ghosts) {
-                        levels.ghosts[g].newFrame();
-                    }
-                }
-            }
-        }
+    if (elapsed > fpsInterval) { // If enough time has elapsed, draw the next frame
+      then = now - (elapsed % fpsInterval);
+      
+      if (!paused) { // GAME LOOP
+        avatar.physics();
+        frame++;
+      }
     }
+  }
 }
 
 ///////////
 // EVENTS
 ///////////
-function keyPressed(code, num) {
-    if (!paused || !num) {
-        if ((code == 37 || code == 65) && !avatar.complete) avatar.keys[0] = num; // Left
-        else if ((code == 38 || code == 87) && !avatar.complete) avatar.keys[1] = num; // Up
-        else if ((code == 39 || code == 68) && !avatar.complete) avatar.keys[2] = num; // Right
-        else if ((code == 40 || code == 83) && !avatar.complete) avatar.keys[3] = num; // Down
-        else if ((code == 69 || code == 32) && num && !avatar.complete) swapTime(); // E or [Space]
-        else if ((code == 80 || code == 82 || code == 27) && num) dom.key(code); // P or R or [Esc]
-    } else if (num) { // keydown on menus
-        dom.key(code);
-    }
+function keyPressed(code, pressed) {
+  if (!paused || !pressed) {
+    if ((code == 37 || code == 65)) return; // Left
+    else if ((code == 38 || code == 87)) return; // Up
+    else if ((code == 39 || code == 68)) return; // Right
+    else if ((code == 40 || code == 83)) return; // Down
+    else if ((code == 80 || code == 82 || code == 27) && pressed) return; // P or R or [Esc]
+  }
 }
 
-document.addEventListener("keydown", function(event) {
-    let k = event.code;
-    if (k == 9 || k == 38 || k == 40) {
-        event.preventDefault();
-    } else if (k == 123 || (event.ctrlKey && event.shiftKey && (k == 73 || k == 74))) {
-        event.preventDefault();
-        return false;
-    }
-    keyPressed(k, 1);
+document.addEventListener("keydown", (event) => {
+  let k = event.code;
+  
+  if (k == 9 || k == 38 || k == 40) {
+    event.preventDefault();
+  } else if (k == 123 || (event.ctrlKey && event.shiftKey && (k == 73 || k == 74))) {
+    event.preventDefault();
+    return false;
+  }
+
+  keyPressed(k, 1);
 }, false);
 
-document.addEventListener("keyup", function(event) {
+document.addEventListener("keyup", (event) => {
 	keyPressed(event.code, 0);
 });
 
-document.addEventListener("mousedown", function (e) { // stops blurring
-    e.preventDefault();
-    return false;
+document.addEventListener("mousedown", (event) => {
+  event.preventDefault();
+  return false;
 });
 
-document.addEventListener('contextmenu', event => event.preventDefault());
+document.addEventListener('contextmenu', (event) => {
+  event.preventDefault();
+});
 
 ///////////
 // WHERE IT ALL STARTS
 ///////////
-window.onload = function () {
+window.onload = () => {
+  // main.js
   GI.init();
   Storage.init();
   Screen.init();
   Assets.init();
+  // script.js
+  Canvases.init();
+  Graphics.init();
   
   // START THE GAME (might delay this later)
-  startAnimating(60); // 60 fps
+  Animator.startAnimating(60); // 60 fps
 }
--- script.js
+++ script.js
@@ -1,11 +1,9 @@
+///////////
 // DOM
-
-let Graphics = {
-
-}
+///////////
 
 let Canvases = {
-  layers: 6, // Total layers on our screen
+  layers: 2, // Total layers on our screen
   contexts: [],
   
   init: () => {
@@ -22,35 +20,11 @@ let Canvases = {
     }
   }
 }
-// Canvas holder
-let contexts = [];
 
-function makeContexts(num) {
-    for (let i = 0; i < num; i++) {
-        let canvas = document.createElement("CANVAS");
-        canvas.id = "Canvas" + i;
-        canvas.width = unit * width;
-        canvas.height = unit * height;
-        document.body.insertBefore(canvas, document.querySelector(".belowCanvases"));
-        let thisCTX = canvas.getContext('2d');
-        thisCTX.imageSmoothingEnabled = false;
-        ctx.push(thisCTX);
-    }
-    let miniC = document.body.querySelector("#LCanvas");
-    miniC.width = unit * width * 0.32;
-    miniC.height = unit * height * 0.32;
-    ctx.push(miniC.getContext('2d'));
+let Graphics = {
+  // HANDLES ACTUALLY DRAWING TO THE Canvases OBJECT!
+  
+  init: () => {
+    // Init the graphics system
+  }
 }
-makeContexts(8);
-ctx[4].globalAlpha = 0.5;
-
-// Image holder
-let img = [];
-function makeImages(srcs) {
-    for (let i = 0; i < srcs.length; i++) {
-        let image = new Image();
-        image.src = graphics + "/" + srcs[i];
-        img.push(image);
-    }
-}
-makeImages(["BlockTileset.png", "Background.png", "AvatarTileset.png", "Objects.png"]);
